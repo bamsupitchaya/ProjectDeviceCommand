fe570f9ac48932423cc9825039e83655ebfea9c2
supitchaya <bamsupitchaya2540@gmail.com>
2019-03-22 15:25:05 +0700

    add envi

---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyDsAI-PL3AbiAEEI6nRi0C6DJnLuR6a7qU",
+        authDomain: "devicecommandproject.firebaseapp.com",
+        databaseURL: "https://devicecommandproject.firebaseio.com",
+        projectId: "devicecommandproject",
+        storageBucket: "devicecommandproject.appspot.com",
+        messagingSenderId: "834747581995"
+    }
+};
---
+++ src/environment/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyDsAI-PL3AbiAEEI6nRi0C6DJnLuR6a7qU",
+        authDomain: "devicecommandproject.firebaseapp.com",
+        databaseURL: "https://devicecommandproject.firebaseio.com",
+        projectId: "devicecommandproject",
+        storageBucket: "devicecommandproject.appspot.com",
+        messagingSenderId: "834747581995"
+    }
+};
